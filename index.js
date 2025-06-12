68c14732719f651d06596cda8d42b80042f4e636
ihsanuddin637 <ihsanuddin0003@gmail.com>
2025-06-12 20:46:54 +0600

    environment setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Backend Server is running on ${port}`);
+});
