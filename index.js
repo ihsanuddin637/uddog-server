6e3b0677796fafcf0d705ec27d9d53ecc80d3f7c
ihsanuddin637 <ihsanuddin0003@gmail.com>
2025-06-15 11:38:32 +0600

    added get request by id

--- index.js
+++ index.js
@@ -77,11 +77,11 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+      app.put("/groupData/:id", async (req, res) => {
+      const id = req.params.id;
+      res.send(result);
+    });
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
